b5c87a4414bce0df274cf3e8324279cbfb2e8cb6
jacrifla <jaquelini_cris@hotmail.com>
2022-12-02 01:26:30 -0300

    add erro ajax

--- js/buscar-filmes.js
+++ js/buscar-filmes.js
@@ -6,13 +6,19 @@ function buscarFilme (){
     let xhr = new XMLHttpRequest()
     xhr.open('GET', 'https://63896bdac5356b25a2ffe3bb.mockapi.io/filmes')
     xhr.addEventListener('load', function(){
-        let answer = xhr.responseText
+        let erroAjax = document.querySelector('#erro-ajax')
+        if(xhr.status == 200){
+            erroAjax.classList.add('invisivel')
+            let answer = xhr.responseText
+            filmes = JSON.parse(answer)
+            filmes.forEach(function(filme){
+                adicionaFilmeTabela(filme)
+            })
+        } else {
+            erroAjax.classList.remove('invisivel')
+        }
+
         
-        filmes = JSON.parse(answer)
-        
-        filmes.forEach(function(filme){
-            adicionaFilmeTabela(filme)
-        })
     })
     xhr.send()
 
